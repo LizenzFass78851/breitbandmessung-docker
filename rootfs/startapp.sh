6357fff5f64ebf84c7ac3f9ec081219bdcf84f94
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2026-05-30 10:54:06 +0000

    refactor: Move app flags to s6 params and simplify start script
    - Add an s6-style params file that emits Chromium flags.
    - Clean up old Breitbandmessung Singleton files for reusing the docker volume after restart or recreate of the docker container without userdata lock error.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -1,2 +1,9 @@
-#!/bin/bash
-exec breitbandmessung --no-sandbox
+#!/bin/sh
+#
+# NOTE: Parameters to pass to Breitbandmessung are defined via the `params` file of the
+#       app service.
+#
+
+rm -rf /config/xdg/config/Breitbandmessung/Singleton*
+
+exec breitbandmessung "$@"
